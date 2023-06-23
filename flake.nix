ff4b4679cebbe2e210c98fdfb3474fe68bbc1232
VojtechStep <vojtechstepancik@outlook.com>
2023-06-24 01:28:37 +0200

    Add Nix

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Assam - Aggregator of lemons";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        selfPkgs = self.packages."${system}";
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "assam";
+
+          inputsFrom = [ selfPkgs.assam ];
+          packages = [ pkgs.zls ];
+        };
+
+        packages.default = selfPkgs.assam;
+        packages.assam = pkgs.callPackage
+          ({ stdenvNoCC
+           , zig
+           , bspwm
+           , xtitle
+           }:
+            stdenvNoCC.mkDerivation {
+              pname = "assam";
+              version = "0.1.0";
+              src = self;
+
+              nativeBuildInputs = [ zig ];
+
+              buildInputs = [
+                xtitle
+                bspwm
+              ];
+
+              dontConfigure = true;
+
+              installPhase = ''
+                runHook preInstall
+                zig build -Drelease-safe -Dcpu=baseline --prefix $out install
+                runHook postInstall
+              '';
+            })
+          { };
+
+      }
+      );
+}
